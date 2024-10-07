f919814f63a43300141650491b69a2425d20ad29
manankumar94 <mortal9193@gmail.com>
2024-10-07 21:07:39 +0530

    Main File

--- server/index.js
+++ server/index.js
@@ -1,9 +1,15 @@
 import express from "express";
 import connectDB from "./config/db.js";
+import router from "./routes/userRoutes.js";
 
 
 const app = express();
 const PORT= 6969;
+
+app.use(express.json());
+
+// for testing only
+app.use("/", router);
 
 connectDB().then( ()=>{
     app.listen(PORT, (err)=>{
