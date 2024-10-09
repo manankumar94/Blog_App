2838bd368c0badbb602c88f5f3419f593c243f15
manankumar94 <mortal9193@gmail.com>
2024-10-09 15:51:07 +0530

    SignIn/SignUp

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,46 +1,89 @@
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 import userModel from "../models/userModel.js";
 
-class AuthController{
-
-    static signUp= async(req, res)=>{
+dotenv.config();
+class AuthController {
+    static signUp = async (req, res) => {
         try {
-            const {username, email, password}= req.body;
-            
-            if(!username || !email || !password || username==="" || password==="" || email===""){
-                return res
-                        .status(400)
-                        .send({Message: "All Fields Are Required"})
+            const { username, email, password } = req.body;
+
+            if (!username || !email || !password || username === "" || password === "" || email === "") {
+                return res.status(400).send({ 
+                    success: false,
+                    Message: "All Fields Are Required" });
             } else {
-                const userExist= await userModel.findOne({email: email});
-                
-                if(!userExist){
-            
-                    const newUser= new userModel({
+                const userExist = await userModel.findOne({ email: email });
+
+                if (!userExist) {
+                    const genSalt = 10;
+                    const hashedPassword = await bcryptjs.hash(password, genSalt);  
+
+                    const newUser = new userModel({
                         username: username,
                         email: email,
-                        password: password,
+                        password: hashedPassword,
                     });
 
-                    const savedUser= await newUser.save();
-                    if(savedUser){
-                        return res
-                                .status(200)
-                                .send({Message: "User Registered Successfully"})
+                    const savedUser = await newUser.save();
+                    if (savedUser) {
+                        return res.status(200).send({ 
+                            success: true,
+                            Message: "User Registered Successfully" });
                     } else {
-                        return res
-                                .status(400)
-                                .send({Message: "User Not Registered"})
+                        return res.status(400).send({ 
+                            success: false,
+                            Message: "User Not Registered" });
                     }
-                } else{
-                    return res
-                        .status(400)
-                        .send({Message: "User Already Exists."})
+                } else {
+                    return res.status(400).send({ 
+                        success: false,
+                        Message: "User Already Exists." });
                 }
             }
         } catch (error) {
-            return res
-                    .status(400)
-                    .send({Message: error})
+            return res.status(500).send({ 
+                success: false,
+                Message: error.message || "An error occurred during registration"});
+        }
+    }
+
+    static signIn= async (req, res)=>{
+        try {
+            const {email, password} = req.body;
+            if(email && password){
+                const isUser= await userModel.findOne({email: email});
+
+                if(!isUser){
+                    return res.status(400).send({ 
+                        success: false,
+                        Message: "User Not found"});
+                } else{
+                    if(isUser.email === email && await bcryptjs.compare(password, isUser.password)){
+
+                        const token= jwt.sign(
+                            {userID: isUser._id},
+                            process.env.SECRET_KEY,
+                            // {expiresIn : "2d"},
+                        );
+
+                        return res.status(200).send({ 
+                            success: true,
+                            Message: "SignIn Successfull",
+                            token
+                        });
+                    }
+                }
+            } else{
+                return res.status(400).send({ 
+                    success: false,
+                    Message: "All Fields Are Required"});
+            }
+        } catch (error) {
+            return res.status(500).send({ 
+                success: false,
+                Message: error.message || "An error occurred during SignIn"});
         }
     }
 }
