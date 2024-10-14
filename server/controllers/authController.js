c2c52ebc7f1b05daf38f6651edfd2da2bb5ba498
manankumar94 <mortal9193@gmail.com>
2024-10-14 14:46:54 +0530

    Authentication Controllers

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -69,6 +69,8 @@ class AuthController {
                         );
 
                         return res.status(200).send({ 
+                            _id: isUser._id,
+                            username: isUser.username,
                             success: true,
                             Message: "SignIn Successfull",
                             token
@@ -86,6 +88,56 @@ class AuthController {
                 Message: error.message || "An error occurred during SignIn"});
         }
     }
+
+    static googleAuth = async (req, res) => {
+        const { name, email, googlePhotoUrl } = req.body;
+        try {
+            const user = await userModel.findOne({ email });
+            if (user) {
+                const token = jwt.sign(
+                    { id: user._id },
+                    process.env.SECRET_KEY
+                );
+                const { password, ...rest } = user._doc;
+                res
+                    .status(200)
+                    .cookie('access_token', token, { httpOnly: true })
+                    .json(rest);
+            } else {
+                const generatedPassword = 
+                    Math.random().toString(36).slice(-8) + 
+                    Math.random().toString(36).slice(-8);
+                const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+                
+                const newUser = new userModel({
+                    username: 
+                        name.toLowerCase().split(' ').join('') +
+                        Math.random().toString(9).slice(-4),
+                    email,
+                    password: hashedPassword,
+                    profilePicture: googlePhotoUrl,
+                });
+                
+                await newUser.save();
+                
+                const token = jwt.sign(
+                    { id: newUser._id },
+                    process.env.SECRET_KEY
+                );
+                const { password, ...rest } = newUser._doc;
+                
+                res
+                    .status(200)
+                    .cookie('access_token', token, { httpOnly: true })
+                    .json(rest);
+            }
+        } catch (error) {
+            return res.status(500).json({ 
+                success: false,
+                message: error.message || "An error occurred during SignIn"
+            });
+        }
+    };
 }
 
 export default AuthController;
